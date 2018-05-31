46a78eb107670e2be530f000d240622a5b763970
Alekscallebat <alcalleb@microsoft.com>
2018-05-31 12:35:13 +0000

    working website

--- model.py
+++ model.py
@@ -10,8 +10,8 @@ def index():
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-def read_excel(path="Maching.xlsx"):
-    xl = pd.ExcelFile("Maching.xlsx")
+def read_excel(path):
+    xl = pd.ExcelFile(path)
     df = xl.parse("Sheet1")
     return df
 
@@ -23,7 +23,7 @@ def match_ratio(df,df2,i,j):
         for key in df.keys() :
             if key not in df2.keys():
                 print("WARNING : NOT SAME KEYS")
-            if key not in ['Heure de debut', 'Heure de fin', 'Adresse de messagerie', 'Nom','Pour finir, peux-tu nous laisser ton prenom / nom ?']:
+            if key not in ['Heure de début', 'Heure de fin', 'Adresse de messagerie', 'Nom','Pour finir, peux-tu nous laisser ton prénom / nom ?']:
                 answer_similarity=[]
                 for answer in df[key][i]:
                     if answer in df2[key][j]:
@@ -65,93 +65,97 @@ def get_all_matches(df1,df2):
     return result_df
 
 def get_top_matches(df):
-    ## We take into input the dataframe with all the matches and output a very good way of allocating df2 names to df1
-    
-    #Initialize the three outputs array as empty
-    nom1,nom2,score=[],[],[]
-
-    #Create a list with the full arrays that we will empty 
-    nom1_original=df["Nom"].values
-    nom2_original=(df.keys().values[1:])
-    score_original=[df[name].values for name in nom1_original]
-
-
-    for j in range(len(df)):
+    try :
+        ## We take into input the dataframe with all the matches and output a very good way of allocating df2 names to df1
         
-        if len(nom1_original)<1 or len(nom2_original)<1:
-            #STOP whenever all df2 names are allocated
-            break
-        else :
-            score_original=[]
-            for name in nom1_original :
-                column=[]
-                for j in range(len(df)):
-                    if df["Nom"].values[j] in nom2_original:
-                        column.append(df[name].values[j])
-                score_original.append(column)
-            #print("new score",len(score_original))
-            nom1_temp,nom2_temp,score_temp=[],[],[]
+        #Initialize the three outputs array as empty
+        nom1,nom2,score=[],[],[]
 
-            # first look to take all the argmaxes
-            for k in range(len(nom1_original)):    
-                i=np.argmax(score_original[:][k])
-                
-                nom1_temp.append(nom1_original[k])
-                nom=nom2_original[i]
-                nom2_temp.append(nom)
-                score_temp.append(score_original[k][i])
-                    
-            ### Uncomment if you allow double matches (yes in that case the code should be simple)
-#            nom1,nom2,score=nom1_temp,nom2_temp,score_temp            
- #           break
+        #Create a list with the full arrays that we will empty 
+        nom1_original=df["Nom"].values
+        nom2_original=(df.keys().values[1:])
+        score_original=[df[name].values for name in nom1_original]
+
+
+        for j in range(len(df)):
             
-            #The idea is to keep all the maximum for a df2 max. It won't always give the optimal solution,
-            # but close enough ("algorithme glouton")
+            if len(nom1_original)<1 or len(nom2_original)<1:
+                #STOP whenever all df2 names are allocated
+                break
+            else :
+                score_original=[]
+                for name in nom1_original :
+                    column=[]
+                    for j in range(len(df)):
+                        if df["Nom"].values[j] in nom2_original:
+                            column.append(df[name].values[j])
+                    score_original.append(column)
+                #print("new score",len(score_original))
+                nom1_temp,nom2_temp,score_temp=[],[],[]
 
-            nom2_temp_unique=[]
-            for k in nom2_temp:
-                if k not in nom2_temp_unique:
-                    nom2_temp_unique.append(k)
+                # first look to take all the argmaxes
+                for k in range(len(nom1_original)):    
+                    i=np.argmax(score_original[:][k])
+                    
+                    nom1_temp.append(nom1_original[k])
+                    nom=nom2_original[i]
+                    nom2_temp.append(nom)
+                    score_temp.append(score_original[k][i])
+                        
+                ### Uncomment if you allow double matches (yes in that case the code should be simple)
+    #            nom1,nom2,score=nom1_temp,nom2_temp,score_temp            
+    #           break
+                
+                #The idea is to keep all the maximum for a df2 max. It won't always give the optimal solution,
+                # but close enough ("algorithme glouton")
 
-            #Here, we associate values and match for each of the maximums (in nom2_temp(_unique))
-            value=dict([(k,0) for k in nom2_temp_unique])
-            match=dict([(k,"") for k in nom2_temp_unique])
+                nom2_temp_unique=[]
+                for k in nom2_temp:
+                    if k not in nom2_temp_unique:
+                        nom2_temp_unique.append(k)
 
-            for k in range(len(nom2_temp)):
-                matcher=nom2_temp[k]
-                matched=nom1_temp[k]
-                ratio=score_temp[k]
-                if ratio>value[matcher] :
-                                   
-                    value[matcher]=ratio
-                    match[matcher]=matched
-                else :
-                    pass
-#          
-            #Here we count the columns and line that have not found an optimal match, and keep them inside score_original
- 
+                #Her    e, we associate values and match for each of the maximums (in nom2_temp(_unique))
+                value=dict([(k,0) for k in nom2_temp_unique])
+                match=dict([(k,"") for k in nom2_temp_unique])
 
-            # Add the validated names to the definitive lists to output
+                for k in range(len(nom2_temp)):
+                    matcher=nom2_temp[k]
+                    matched=nom1_temp[k]
+                    ratio=score_temp[k]
+                    if ratio>value[matcher] :
+                                    
+                        value[matcher]=ratio
+                        match[matcher]=matched
+                    else :
+                        pass
+    #          
+                #Here we count the columns and line that have not found an optimal match, and keep them inside score_original
+    
 
-            nom1=np.concatenate((nom1,[match[j] for j in nom2_temp_unique]))
-            nom2=np.concatenate((nom2,[k for k in nom2_temp_unique]))
-            score=np.concatenate((score,[value[k] for k in nom2_temp_unique]))
+                # Add the validated names to the definitive lists to output
 
-            buffer=[]
-            for k in nom1_original :
-                if k not in nom1:
-                    buffer.append(k)
-            nom1_original=buffer
-            buffer=[]
-            for k in nom2_original:
-                if k not in nom2:
-                    buffer.append(k)
-            nom2_original=buffer
+                nom1=np.concatenate((nom1,[match[j] for j in nom2_temp_unique]))
+                nom2=np.concatenate((nom2,[k for k in nom2_temp_unique]))
+                score=np.concatenate((score,[value[k] for k in nom2_temp_unique]))
 
+                buffer=[]
+                for k in nom1_original :
+                    if k not in nom1:
+                        buffer.append(k)
+                nom1_original=buffer
+                buffer=[]
+                for k in nom2_original:
+                    if k not in nom2:
+                        buffer.append(k)
+                nom2_original=buffer
 
-    df_top=pd.DataFrame({"nom1":nom1,"nom2":nom2,"score":score})
-    return df_top
-UPLOAD_FOLDER = 'uploads'
+        df_top=pd.DataFrame({"nom1":nom1,"nom2":nom2,"score":score})
+        return df_top
+    except:
+        print(df)
+        return df
+
+UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(["csv", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "xlsx"])
 
 app = Flask(__name__)
@@ -179,10 +183,14 @@ def upload_file():
             file2.save(os.path.join(app.config['UPLOAD_FOLDER'], "liste2.xlsx"))
             df=(read_excel(os.path.join(app.config['UPLOAD_FOLDER'], "liste1.xlsx")))
             df2=(read_excel(os.path.join(app.config['UPLOAD_FOLDER'], "liste2.xlsx")))
+            #print("check formating",df["Nom"], df2["Nom"])
             if len(df)<len(df2):
+                print("case 1")
                 df=get_all_matches(df,df2)
             else:
+                print("case 2")
                 df=get_all_matches(df2,df)
+                print(df)
             df_top=get_top_matches(df)
             return render_template('view.html',tables=[df_top.to_html(classes="df"),df.to_html(classes="df")],
     titles = ['na',"Meilleure configuration", 'TOUS les resultats'])
